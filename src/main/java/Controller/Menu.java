c5ac0ca8ce569fb1a8c1d794cb1b878a330e4212
yousra_chaieb <100976916+yousra0@users.noreply.github.com>
2024-05-03 20:33:48 +0100

    new commit

---
+++ src/main/java/Controller/Menu.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Menu {
+    public void openPost(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/AfficherPost.fxml"));
+        try {
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
